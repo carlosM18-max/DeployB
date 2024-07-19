775ce72b7455766235b70686a2b4510880cbfb50
carlosM18-max <carloshernandez74199@gmail.com>
2024-07-19 14:08:03 -0600

    Despliege de la Base de Datos

--- config/db.py
+++ config/db.py
@@ -2,7 +2,7 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
-SQLALCHEMY_DATABASE_URL = "mysql+pymysql://root:1234@localhost:3307/hospital"
+SQLALCHEMY_DATABASE_URL = "mysql+pymysql://avnadmin:AVNS__1cZtouYw6xF5XKhFir@mysql-167320d6-carloshernandez74199-f967.e.aivencloud.com:23040/defaultdb"
 engine = create_engine(SQLALCHEMY_DATABASE_URL)
 SessionLocal = sessionmaker(autocommit = False, autoflush = False, bind = engine)
 Base = declarative_base()
